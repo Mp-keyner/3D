91676d6440ef76d61189fe18198853d0c13f89e3
KiuraAdmin <support@kiura.co>
2024-04-11 16:18:16 -0500

    11 de abril 4:18 PM

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StlViewer } from "react-stl-viewer";
+// import stld from "./assets/Baby_Yoda_v2.2.stl";
+import stld from "./assets/Octocat-v2.stl";
+
+const App = () => {
+  const style = {
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+  };
+  return (
+    <>
+      <StlViewer style={style} orbitControls shadows url={stld} />
+    </>
+  );
+};
+
+export default App;
